3a5d50964f8df4ff186ee4db0ceab687d98e80ee
Odingoii <okoromaloba@gmail.com>
2024-12-08 05:59:19 +0300

    Session redirect

--- missions/session.php
+++ missions/session.php
@@ -4,7 +4,7 @@ session_start(); // Start the session
 // Check if the user is not logged in
 if (!isset($_SESSION['user_id'])) {
     // Redirect the user to the login page
-    header("Location: pages/signin.php");
+    header("Location: ../login.php");
     exit(); // Stop executing the current script
 }
 ?>
