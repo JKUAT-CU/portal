62c17dfbc1dc352c3d557ad092ab3ec45c9df16f
Odingoii <okoromaloba@gmail.com>
2024-12-20 23:47:25 +0300

    Honestly, i'd rather not say

--- backend/db.php
+++ backend/db.php
@@ -1,16 +1,16 @@
 <?php
 
 // // Database connection
-// $host = 'localhost';
-// $user = 'jkuatcu_devs';
-// $password = '#God@isAble!#';  // Ensure this is the correct password
-// $database = 'jkuatcu_data';
+$host = 'localhost';
+$user = 'jkuatcu_devs';
+$password = '#God@isAble!#';  // Ensure this is the correct password
+$database = 'jkuatcu_data';
 
 // // Database connection
-$host = 'localhost';
-$user = 'portals';
-$password = 'I&Y*U&^(JN&Y Kjbkjn'; // Ensure this is the correct password
-$database = 'jkuatcu_data';
+// $host = 'localhost';
+// $user = 'portals';
+// $password = 'I&Y*U&^(JN&Y Kjbkjn'; // Ensure this is the correct password
+// $database = 'jkuatcu_data';
 
 // Create connection
 $mysqli = new mysqli($host, $user, $password, $database);
